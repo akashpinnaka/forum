1ccc7d46128c1d50b4aad25d0f1a88a397701fc8
akashpinnaka <aakash.pinnaka@gmail.com>
2015-11-15 23:10:44 +0530

    Generate controller for Posts and add CRUD for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+
+  before_action :find_post, :only => [:show, :edit, :update, :delete, :destroy]
+
+  def index
+    @posts = Post.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to(:action => "index")
+    else
+      render("new")
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to(:action => "index")
+    else
+      render("edit")
+    end
+  end
+
+  def delete
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to(:action => "index")
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+end
