b7ff94958e3cf199b5ced3df3710403267cc112c
akashpinnaka <aakash.pinnaka@gmail.com>
2015-12-03 12:39:23 +0530

    useless commit for master branch

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  
+
+	def create
+		@comment = Comment.new(comment_params)
+		@comment.save
+		redirect_to(:controller => "posts", :action => "show", :id => @comment.post_id)    
+	end
+
+
+
+	private
+
+	
+
+	def comment_params
+		params.require(:comment).permit(:answer, :post_id, :user_id)
+	end
+
+
+
+end
