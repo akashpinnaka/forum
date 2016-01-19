8482d19b70ffba43a05fc2779fca3a3e193caf4d
akashpinnaka <aakash.pinnaka@gmail.com>
2016-01-19 21:26:09 +0530

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 
 	has_many :blocks
 	has_many :blockers, :through => "blocks"
-	has_many :blockies, :through => "blocks", :source => "blockie"
+	has_many :blockies, :through => "blocks"
 
 	has_attached_file :profile_picture, :styles => { :medium => "300x300>", :thumb => "100x100#" }
   	validates_attachment_content_type :profile_picture, content_type: /\Aimage\/.*\Z/
